53cf2a2d802cf49738d22fed268e42189ca9d547
kaefd <ikfdla13@gmail.com>
2024-02-08 13:26:05 +0700

    first commit

--- server.js
+++ server.js
@@ -3,12 +3,11 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
-const sequelize = require('./connect'); // Pastikan untuk mengonfigurasi koneksi database di sini
+const sequelize = require('./connect');
 const routes = require('./src/routes/routes');
 const config = require('./src/middleware/config');
 
-// Sinkronkan model dengan database
-sequelize.sync({ force: false }) // Sesuaikan parameter force sesuai kebutuhan Anda
+sequelize.sync({ force: false })
   .then(() => {
     console.log('Database synchronized');
   })
@@ -16,7 +15,6 @@ sequelize.sync({ force: false }) // Sesuaikan parameter force sesuai kebutuhan A
     console.error('Error synchronizing database:', error);
   });
 
-// Middleware untuk parsing JSON
 app.use(express.json());
 app.use(session({
   secret: config.secret,
@@ -30,10 +28,8 @@ app.use((req, res, next) => {
   next()
 })
 
-// Menggunakan rute-rute
 app.use('/api', routes);
 
-// Port untuk server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
